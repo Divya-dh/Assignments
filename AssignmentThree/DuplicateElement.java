5ff9c27dd04f74d14c73a975c3ad1331dbc9465a
Divya-dh <divyadharshini5603@gmail.com>
2025-05-28 19:04:34 +0530

    Update DuplicateElement.java

--- AssignmentThree/DuplicateElement.java
+++ AssignmentThree/DuplicateElement.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class DuplicateElement{
+public class DuplicateElement {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
